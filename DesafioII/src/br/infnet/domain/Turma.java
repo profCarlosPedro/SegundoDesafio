4b265bb8f8d7cae2ada3e36c59833e359fe5f3a4
Carlos Pedro <carlos.muniz@prof.infnet.edu.br>
2014-03-10 23:04:51 -0300

    Segundo Desafio

---
+++ DesafioII/src/br/infnet/domain/Turma.java
@@ -0,0 +1,32 @@
+package br.infnet.domain;
+
+import java.util.List;
+
+public class Turma {
+
+	private List<Aluno> alunos;
+	private int id;
+	private String nome;
+	public List<Aluno> getAlunos() {
+		return alunos;
+	}
+	public void setAlunos(List<Aluno> alunos) {
+		this.alunos = alunos;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+	
+}
